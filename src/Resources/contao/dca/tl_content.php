23aa57aafb3854532c1f75ab628e011cf79beb7d
lautschrift <steffen@lautschrift.com>
2019-02-11 17:42:48 +0100

    Update tl_content.php

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -1,31 +1,38 @@
 <?php
-
+// dca/tl_content.php
 /**
  * Table tl_content
  */
 $strName = 'tl_content';
 
+
 /* Palettes */
-$GLOBALS['TL_DCA'][$strName]['palettes']['masonrypic'] = '{type_legend},type,headline;{description_legend,}ls_masonry_properties,ls_masonry_link;{invisible_legend:hide},invisible,start,stop;'
-
-
-
+$GLOBALS['TL_DCA'][$strName]['palettes']['myproduct'] = '{type_legend},type,headline;{description_legend},productdescription,productproperties,productprice;{invisible_legend:hide},invisible,start,stop;';
 
 
 /* Fields */
-$GLOBALS['TL_DCA'][$strName]['fields']['ls_masonry_properties'] = array
+$GLOBALS['TL_DCA'][$strName]['fields']['productdescription'] = array
 (
-    'label'                   => &$GLOBALS['TL_LANG'][$strName]['ls_masonry_properties'],
+    'label'                   => &$GLOBALS['TL_LANG'][$strName]['productdescription'],
     'exclude'                 => true,
-    'inputType'               => 'text',
+    'inputType'               => 'textarea',
+    'eval'                    => array('rte' => 'tinyMCE'),
     'sql'                     => "text NOT NULL"
 );
 
-$GLOBALS['TL_DCA'][$strName]['fields']['ls_masonry_link'] = array
+$GLOBALS['TL_DCA'][$strName]['fields']['productproperties'] = array
 (
-    'label'                   => &$GLOBALS['TL_LANG'][$strName]['ls_masonry_link'],
+    'label'                   => &$GLOBALS['TL_LANG'][$strName]['productproperties'],
+    'exclude'                 => true,
+    'inputType'               => 'listWizard',
+    'sql'                     => "text NOT NULL"
+);
+
+$GLOBALS['TL_DCA'][$strName]['fields']['productprice'] = array
+(
+    'label'                   => &$GLOBALS['TL_LANG'][$strName]['productprice'],
     'exclude'                 => true,
     'inputType'               => 'text',
-    'eval'                    => array('rgxp'=>'url', 'decodeEntities'=>true, 'maxlength'=>255, 'dcaPicker'=>true, 'tl_class'=>'w50 wizard'),
+    'eval'                    => array('tl_class' => 'w50'),
     'sql'                     => "varchar(255) NOT NULL default ''"
 );
