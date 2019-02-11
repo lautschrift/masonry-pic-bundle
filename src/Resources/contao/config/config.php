00bc0f56412b401add4baf7c7b80a810743447fe
lautschrift <steffen@lautschrift.com>
2019-02-11 22:50:44 +0100

    Wip

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -2,3 +2,6 @@
 <?php
 // config/config.php
 $GLOBALS['TL_CTE']['masnonry']['masonrypic'] = 'Lautschrift\\MasonryPicBundle\\Resources\\contao\\classes\\MasonryPic';
+
+// Frontend modules
+$GLOBALS['FE_MOD']['masonry']['masonrypic'] = 'Lautschrift\MasonryPicBundle\Module\MasonryPicModule';
