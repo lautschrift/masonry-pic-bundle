eb82161bd3e1dae0bf00f7430553d6f656d10805
lautschrift <steffen@lautschrift.com>
2019-02-11 21:37:57 +0100

    Create tl_content.php

---
+++ src/Resources/contao/languages/de/tl_content.php
@@ -0,0 +1,18 @@
+
+<?php
+// languages/de/tl_content.php
+/**
+ * Table: tl_content
+ */
+$strName = 'tl_content';
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG'][$strName]['ls_masonry_properties'] = array('Beschreibung', 'Bitte geben Sie die Bild-Beschreibung ein.');
+$GLOBALS['TL_LANG'][$strName]['ls_masonry_link']  = array('Link', 'Bitte geben Sie den Link für die Folgeseite ein.');
+
+/**
+ * Legends
+ */
+$GLOBALS['TL_LANG'][$strName]['description_legend'] = 'Overlay Text';
