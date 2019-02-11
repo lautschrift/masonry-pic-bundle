c635c01d01323e54d0c1101ed32429b4d1e9cc33
lautschrift <steffen@lautschrift.com>
2019-02-12 00:01:59 +0100

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,7 +1,8 @@
 
 <?php
 // config/config.php
-$GLOBALS['TL_CTE']['masnonry']['masonrypic'] = 'Lautschrift\\MasonryPicBundle\\Resources\\contao\\classes\\MasonryPic';
+//$GLOBALS['TL_CTE']['masnonry']['masonrypic'] = 'Lautschrift\\MasonryPicBundle\\Resources\\contao\\classes\\MasonryPic';
+$GLOBALS['TL_CTE']['masnonry']['masonrypic'] = 'Lautschrift\\MasonryPicBundle\\Modeule\\MasonryPicModule';
 
 // Frontend modules
 //$GLOBALS['FE_MOD']['masonry']['masonrypic'] = 'Lautschrift\\MasonryPicBundle\\Module\\MasonryPicModule';
