a2dc0ebf13d96bc4de483c3de315ccf0e454d979
lautschrift <steffen@lautschrift.com>
2019-02-11 17:35:26 +0100

    Update tl_content.php

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -5,71 +5,27 @@
  */
 $strName = 'tl_content';
 
+/* Palettes */
+$GLOBALS['TL_DCA'][$strName]['palettes']['masonrypic'] = '{type_legend},type,headline;{description_legend,}ls_masonry_properties,ls_masonry_link;{invisible_legend:hide},invisible,start,stop;'
 
-$GLOBALS['TL_DCA'][$strName] = array
+
+
+
+
+/* Fields */
+$GLOBALS['TL_DCA'][$strName]['fields']['ls_masonry_properties'] = array
 (
-
-    /* Palettes */
-    'palettes' => array
-    (
-        
-        'default'       => '{type_legend},type,headline;{description_legend,}ls_masonry_properties,ls_masonry_link;{invisible_legend:hide},invisible,start,stop;'
-    ),
-
-    // Subpalettes
-    'subpalettes' => array
-    (
-        ''                            => ''
-    ),
-
-    /* Fields */
-    'fields' => array
-    (
-
-        'ls_masonry_description' => array
-        (
-            'label'                   => &$GLOBALS['TL_LANG'][$strName]['ls_masonry_description'],
-            'exclude'                 => true,
-            'inputType'               => 'text',
-            'sql'                     => "text NOT NULL"
-        ),
-
-        'ls_masonry_link' => array
-        (
-            'label'                   => &$GLOBALS['TL_LANG'][$strName]['ls_masonry_properties'],
-            'exclude'                 => true,
-            'inputType'               => 'text',
-            'eval'                    => array('rgxp'=>'url', 'decodeEntities'=>true, 'maxlength'=>255, 'dcaPicker'=>true, 'tl_class'=>'w50 wizard'),
-            'sql'                     => "varchar(255) NOT NULL default ''"
-        )
-
-    )
-);
-
-
-
-/* Fields
-$GLOBALS['TL_DCA'][$strName]['fields']['description'] = array
-(
-    'label'                   => &$GLOBALS['TL_LANG'][$strName]['description'],
+    'label'                   => &$GLOBALS['TL_LANG'][$strName]['ls_masonry_properties'],
     'exclude'                 => true,
     'inputType'               => 'text',
     'sql'                     => "text NOT NULL"
 );
 
-$GLOBALS['TL_DCA'][$strName]['fields']['linkto'] = array
+$GLOBALS['TL_DCA'][$strName]['fields']['ls_masonry_link'] = array
 (
-    'label'                   => &$GLOBALS['TL_LANG'][$strName]['properties'],
+    'label'                   => &$GLOBALS['TL_LANG'][$strName]['ls_masonry_link'],
     'exclude'                 => true,
-    'inputType'               => 'hyperlink',
-    'sql'                     => "text NOT NULL"
+    'inputType'               => 'text',
+    'eval'                    => array('rgxp'=>'url', 'decodeEntities'=>true, 'maxlength'=>255, 'dcaPicker'=>true, 'tl_class'=>'w50 wizard'),
+    'sql'                     => "varchar(255) NOT NULL default ''"
 );
-
-$GLOBALS['TL_DCA'][$strName]['fields']['pic'] = array
-(
-    'label'                   => &$GLOBALS['TL_LANG'][$strName]['pic'],
-    'exclude'                 => true,
-    'inputType'               => 'fileTree',
-    'sql'                     => "text NOT NULL"
-);
-*/
