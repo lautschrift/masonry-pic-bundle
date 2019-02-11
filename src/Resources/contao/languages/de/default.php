cc46b34855a738ed54855233c92819212b520611
lautschrift <31650137+lautschrift@users.noreply.github.com>
2019-02-11 09:25:01 +0100

    Add files via upload

---
+++ src/Resources/contao/languages/de/default.php
@@ -0,0 +1,8 @@
+
+<?php
+// languages/de/default.php
+/**
+ * Content Elements
+ */
+$GLOBALS['TL_LANG']['CTE']['masnonry']      = array('Masonry');
+$GLOBALS['TL_LANG']['CTE']['masonrypic']    = array('Masonry Bild');
