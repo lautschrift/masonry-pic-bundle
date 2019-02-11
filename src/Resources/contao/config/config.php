17f484ad6c0ffc48c889c62fa7ab35862180e6d1
lautschrift <31650137+lautschrift@users.noreply.github.com>
2019-02-11 09:16:29 +0100

    Add files via upload

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,4 +1,4 @@
 
 <?php
 // config/config.php
-$GLOBALS['TL_CTE']['masnonry']['masonrypic'] = 'Lautschrift\\MasonryPicBundle\\Resources\\contao\\classes\\MasonryPic'
+$GLOBALS['TL_CTE']['masnonry']['masonrypic'] = 'Lautschrift\\MasonryPicBundle\\Resources\\contao\\classes\\MasonryPic';
