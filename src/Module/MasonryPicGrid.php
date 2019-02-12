404cb3ad5c4b4378a7f0e54afe8c11620723b258
lautschrift <steffen@lautschrift.com>
2019-02-12 11:17:20 +0100

    Wip

---
+++ src/Module/MasonryPicGrid.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Lautschrift\MasonryPicBundle\Module;
+
+class MasonryPicGrid extends \ContentElement
+{
+    /**
+     * @var string
+     */
+    protected $strTemplate = 'fe_masonryGrid';
+
+    /**
+     * Displays a wildcard in the back end.
+     *
+     * @return string
+     */
+    public function generate()
+    {
+        if (TL_MODE == 'BE') {
+            $template = new \BackendTemplate('be_wildcard');
+
+            //$template->wildcard = '### '.utf8_strtoupper($GLOBALS['TL_LANG']['FMD']['masonrypic'][0]).' ###';
+            $template->wildcard = "### MasonryGrid ###";
+            $template->title = $this->headline;
+            $template->id = $this->id;
+            $template->link = $this->name;
+            $template->href = 'contao/main.php?do=themes&amp;table=tl_content&amp;act=edit&amp;id='.$this->id;
+
+            return $template->parse();
+        }
+
+        return parent::generate();
+    }
+
+    /**
+     * Generates the module.
+     */
+
+     /**
+     * Erzeugt die Ausgebe für das Frontend.
+     * @return string
+     */
+
+    protected function compile()
+    {
+        if ($this->ls_masonry_properties != '') {
+            $this->Template->arrProperties = deserialize($this->ls_masonry_properties, true);
+        }
+    }
+}
---
+++ src/Resources/contao/dca/tl_contentGrid.php
@@ -0,0 +1,10 @@
+<?php
+// dca/tl_content.php
+/**
+ * Table tl_content
+ */
+$strName = 'tl_content';
+
+
+/* Palettes */
+$GLOBALS['TL_DCA'][$strName]['palettes']['masonrypic'] = '{nothing_legend}';
