86105618ecca5a2e54e35c8bcf75f386a2961b24
lautschrift <steffen@lautschrift.com>
2019-02-12 11:57:43 +0100

    Wip

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -2,7 +2,8 @@
 <?php
 // config/config.php
 //$GLOBALS['TL_CTE']['masnonry']['masonrypic'] = 'Lautschrift\\MasonryPicBundle\\Resources\\contao\\classes\\MasonryPic';
-$GLOBALS['TL_CTE']['masnonry']['masonrygrid'] = 'Lautschrift\\MasonryPicBundle\\Module\\MasonryPicGrid';
+$GLOBALS['TL_CTE']['masnonry']['masonrygrid_start'] = 'Lautschrift\\MasonryPicBundle\\Module\\MasonryPicGrid';
+$GLOBALS['TL_CTE']['masnonry']['masonrygrid_end'] = 'Lautschrift\\MasonryPicBundle\\Module\\MasonryPicGridEnd';
 $GLOBALS['TL_CTE']['masnonry']['masonrypic'] = 'Lautschrift\\MasonryPicBundle\\Module\\MasonryPicModule';
 
 // Frontend modules
--- src/Resources/contao/languages/de/default.php
+++ src/Resources/contao/languages/de/default.php
@@ -6,3 +6,5 @@
  */
 $GLOBALS['TL_LANG']['CTE']['masnonry']      = array('Masonry');
 $GLOBALS['TL_LANG']['CTE']['masonrypic']    = array('Masonry Bild');
+$GLOBALS['TL_LANG']['CTE']['masonrygrid_start']    = array('Masonry Grid Start');
+$GLOBALS['TL_LANG']['CTE']['masonrygrid_end']    = array('Masonry Grid Ende');
